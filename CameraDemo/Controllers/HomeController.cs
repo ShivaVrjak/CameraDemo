8e1a2065270c058f75275d84f0ea36fc8c871932
ram_bt <ram@braintechnosys.com>
2020-12-10 16:25:44 +0530

    Add project files.

---
+++ CameraDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CameraDemo.Models;
+using System.Web.Mvc;
+
+namespace CameraDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetQRCode(string qrCode)
+        {
+            QRCodeViewModel model = new QRCodeViewModel() { QRCodeInfo=qrCode};
+            return View(model);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
